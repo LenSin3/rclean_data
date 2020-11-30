1fe80e383a373d8032df19c7a1b2c7922ea1b437
LenSin3 <lenpk3@yahoo.co.uk>
2020-11-29 21:26:34 -0800

    Created data frame from train data, activity and subject labels

---
+++ run_analysis.R
@@ -0,0 +1,34 @@
+# Get Data
+# Download the data in the current working directory
+
+library(data.table)
+filepath = 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
+if (!file.exists('.\\UCI HAR Dataset.zip')){
+  download.file(filepath,'.\\UCI HAR Dataset.zip', mode = 'wb')
+  unzip("UCI HAR Dataset.zip", exdir = getwd())
+}
+
+
+# Read the data set and merge into a single data frame
+# Import the features
+features <- read.csv( '.\\UCI HAR Dataset\\features.txt', header = FALSE, sep = ' ')
+features <- as.character(features[,2])
+
+# Train Data
+
+# Import train data set
+train_data_x <- read.table('.\\UCI HAR Dataset\\train\\X_train.txt', header=FALSE)
+
+# Import the activity labels
+train_activity <- read.csv('.\\UCI HAR Dataset\\train\\y_train.txt', header=FALSE)
+
+# Import the subject labels
+train_subject <- read.csv('.\\UCI HAR Dataset\\train\\subject_train.txt', header=FALSE)
+
+# Merge into data frame train_dataset
+train_dataset <- data.frame(train_subject, train_activity, train_data_x)
+names(train_dataset) <- c(c('subject', 'activity'), features)
+# head(train_dataset)
+
+# Test Data
+
